b4611a35ff85c5b6ad2b16c3e60b35834e6a6e2d
Mister-Hope <mister-hope@outlook.com>
2023-02-23 14:10:06 +0800

    5 files modified

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # online-demo
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuepress-theme-hope)
+[Fork on StackBlitz ⚡️](https://stackblitz.com/fork/vuepress-theme-hope)
+
+## Docs Demo
+
+- Preview: `npm run docs:dev`
+- Build: `npm run docs:build`
+
+## Blog Demo
+
+- Preview: `npm run blog:dev`
+- Build: `npm run blog:build`
--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -1,93 +1,72 @@
-import { hopeTheme } from "vuepress-theme-hope";
-import { enNavbar, zhNavbar } from "./navbar/index.js";
-import { enSidebar, zhSidebar } from "./sidebar/index.js";
+import { hopeTheme } from 'vuepress-theme-hope';
+import { enNavbar, zhNavbar } from './navbar/index.js';
+import { enSidebar, zhSidebar } from './sidebar/index.js';
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: 'https://vuepress-theme-hope-docs-demo.netlify.app',
 
   author: {
-    name: "Mr.Hope",
-    url: "https://mrhope.site",
+    name: 'Mr.Hope',
+    url: 'https://mrhope.site',
   },
 
-  iconAssets: "iconfont",
+  iconAssets: 'iconfont',
 
-  logo: "/logo.svg",
+  logo: '/logo.svg',
 
-  repo: "vuepress-theme-hope/online-demo",
+  repo: 'vuepress-theme-hope/online-demo',
 
-  docsDir: "docs",
+  docsDir: 'docs',
 
   locales: {
-    "/": {
+    '/': {
       // navbar
       navbar: enNavbar,
 
       // sidebar
       sidebar: enSidebar,
 
-      footer: "Default footer",
+      footer: 'Default footer',
 
       displayFooter: true,
 
       metaLocales: {
-        editLink: "Edit this page on GitHub",
+        editLink: 'Edit this page on GitHub',
       },
     },
 
     /**
      * Chinese locale config
      */
-    "/zh/": {
+    '/zh/': {
       // navbar
       navbar: zhNavbar,
 
       // sidebar
       sidebar: zhSidebar,
 
-      footer: "默认页脚",
+      footer: '默认页脚',
 
       displayFooter: true,
 
       // page meta
       metaLocales: {
-        editLink: "在 GitHub 上编辑此页",
+        editLink: '在 GitHub 上编辑此页',
       },
     },
   },
 
   encrypt: {
     config: {
-      "/demo/encrypt.html": ["1234"],
-      "/zh/demo/encrypt.html": ["1234"],
+      '/demo/encrypt.html': ['1234'],
+      '/zh/demo/encrypt.html': ['1234'],
     },
   },
 
   plugins: {
-    // If you don’t need comment feature, you can remove following option
-    // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
-    // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
     comment: {
-      /**
-       * Using Giscus
-       */
-      // provider: "Giscus",
-      // repo: "vuepress-theme-hope/giscus-discussions",
-      // repoId: "R_kgDOG_Pt2A",
-      // category: "Announcements",
-      // categoryId: "DIC_kwDOG_Pt2M4COD69",
-
-      /**
-       * Using Twikoo
-       */
-      // provider: "Twikoo",
-      // envId: "https://twikoo.ccknbc.vercel.app",
-
-      /**
-       * Using Waline
-       */
-      provider: "Waline",
-      serverURL: "https://vuepress-theme-hope-comment.vercel.app",
+      // @ts-ignore
+      provider: 'Waline',
     },
 
     // Disable features you don’t want here
@@ -109,20 +88,20 @@ export default hopeTheme({
       mark: true,
       mermaid: true,
       playground: {
-        presets: ["ts", "vue"],
+        presets: ['ts', 'vue'],
       },
       presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
+        plugins: ['highlight', 'math', 'search', 'notes', 'zoom'],
       },
       stylize: [
         {
-          matcher: "Recommended",
+          matcher: 'Recommended',
           replacer: ({ tag }) => {
-            if (tag === "em")
+            if (tag === 'em')
               return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
+                tag: 'Badge',
+                attrs: { type: 'tip' },
+                content: 'Recommended',
               };
           },
         },
